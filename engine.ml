9a34c65fe8e65298d00d96833feb4049f6bb7ba4
kdazzle1 <kevinshen8@optonline.net>
2015-11-27 20:37:05 -0500

    Processing and update

--- engine.ml
+++ engine.ml
@@ -5,7 +5,7 @@ type attack = Left | Right | Down | Up | Neutral
 
 type move = MLeft | MRight | MDown | MUp
 
-(**let character = [create Light (100,100)]*)
+(*let characters = (create Light (100,100),create Medium (100,100))*)
 
 let lastmove = ref [None;None]
 
@@ -28,5 +28,29 @@ let rec input_loop () =
 let start_engine () =
   let _ = Thread.create input_loop () in
   Thread.join (Thread.create tickprocessor ())
+
+let process_move (m: move) (i: int) : unit =
+  match m with
+  | MLeft ->
+    if i = 0 then
+      ((fst characters).velocity <-
+        ((fst characters).speed * (-1),snd (fst characters).velocity);
+      ())
+    else
+      ((snd characters).velocity <-
+        ((snd characters).speed * (-1),snd (snd characters).velocity);
+      ())
+  | MRight -> failwith "TODO"
+  | _ -> failwith "TODO"
+
+let update () =
+  (fst characters).pos <-
+    (fst (fst characters).velocity + fst (fst characters).pos,
+     snd (fst characters).velocity + snd (fst characters).pos);
+  (snd characters).pos <-
+    (fst (snd characters).velocity + fst (snd characters).pos,
+     snd (snd characters).velocity + snd (snd characters).pos);
+  ()
+
 
 let _ = start_engine ()
