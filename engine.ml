9ca0f9d814d6d0d869c5939778df3454cc88f591
sahilkanjiyani <ssk255@cornell.edu>
2015-11-28 23:26:00 -0500

    stage collisions,jumps,and air boolean considered, stuns left

--- engine.ml
+++ engine.ml
@@ -7,23 +7,87 @@ type move = MLeft | MRight | MDown | MUp
 
 let characters = (create Light {x=100;y=100},create Medium {x=100;y=100})
 
-let lastmove = ref (MDown,1,MDown,1)
+let ch1width = get_width (fst characters)
+let ch2width = get_width (snd characters)
 
+let lastmove = ref (MDown,1,MDown,1)
+(**These contants need to be set correctly for engine to work*)
 let fallconstant = -2
 
 let gravity = 5
 
 let jumpconstant = 1
 
+let stagetop = 100
+
+let stageleft = 100
+
+let stageright = 200
+
 let newinputs = ref []
 
 let _ = open_graph ""
 
-let update () = (**need to know when we hit stage, probably hardcoded coordinates of it vertically and horizontally*)
-  let newpos1 = {x=(fst characters).velocity.x + (fst characters).pos.x;
-     y=(fst characters).velocity.y + (fst characters).pos.y} in
-  let newpos2 = {x=(snd characters).velocity.x + (snd characters).pos.x;
-     y=(snd characters).velocity.y + (snd characters).pos.y} in
+let stagecollision pos1 pos2 =
+  let topdiff1 = pos1.y - stagetop in
+  let topdiff2 = pos2.y - stagetop in
+  let rightdiff1 = pos1.x - stageright in
+  let rightdiff2 = pos2.x - stageright in
+  let leftdiff1 = stageleft - (pos1.x + ch1width) in
+  let leftdiff2 = stageleft - (pos2.x + ch2width) in
+  (if topdiff1 < 0 then
+    if leftdiff1 < 0 && rightdiff1 < 0 then
+      if topdiff1 > leftdiff1 && topdiff1 > rightdiff1 then
+      ((fst characters).air <- false ;
+      (fst characters).jumps <- 2 ;
+      change_velocity (fst characters) {x=0;y=0} ;
+      moveto (fst characters) {x=pos1.x;y=stagetop})
+      else
+        if leftdiff1 > rightdiff1 then
+          (change_velocity (fst characters) {x=0;y= (fst characters).velocity.y} ;
+          moveto (fst characters) {x=stageleft-ch1width;y=(fst ((fst characters).hitbox)).y} )
+        else
+          (change_velocity (fst characters) {x=0;y= (fst characters).velocity.y} ;
+          moveto (fst characters) {x=stageright;y=(fst ((fst characters).hitbox)).y} )
+    else
+      ()
+  else
+    if topdiff1 = 0 && (leftdiff1 > 0 || rightdiff1 > 0) then
+     (fst characters).air <- true
+     else
+     ()) ;
+  if topdiff2 < 0 then
+    if leftdiff2 < 0 && rightdiff2 < 0 then
+      if topdiff2 > leftdiff2 && topdiff2 > rightdiff2 then
+      ((snd characters).air <- false ;
+      (snd characters).jumps <- 2 ;
+      change_velocity (snd characters) {x=0;y=0} ;
+      moveto (snd characters) {x=pos2.x;y=stagetop})
+      else
+        if leftdiff2 > rightdiff2 then
+          (change_velocity (snd characters) {x=0;y= (snd characters).velocity.y} ;
+          moveto (snd characters) {x=stageleft-ch2width;y=(fst ((snd characters).hitbox)).y} )
+        else
+          (change_velocity (snd characters) {x=0;y= (snd characters).velocity.y} ;
+          moveto (snd characters) {x=stageright;y=(fst ((snd characters).hitbox)).y} )
+    else
+      ()
+  else
+    if topdiff2 = 0 && (leftdiff2 > 0 || rightdiff2 > 0) then
+     (snd characters).air <- true
+     else
+     ()
+
+let update () = (**velocity changes differently when stunned*)
+  let newpos1test = {x=(fst characters).velocity.x + (fst ((fst characters).hitbox)).x;
+     y=(fst characters).velocity.y + (fst ((fst characters).hitbox)).y} in
+  let newpos2test = {x=(snd characters).velocity.x + (fst ((snd characters).hitbox)).x;
+     y=(snd characters).velocity.y + (fst ((snd characters).hitbox)).y} in
+  stagecollision newpos1test newpos2test ;
+  let newpos1 = {x=(fst characters).velocity.x + (fst ((fst characters).hitbox)).x;
+     y=(fst characters).velocity.y + (fst ((fst characters).hitbox)).y} in
+  let newpos2 = {x=(snd characters).velocity.x + (fst ((snd characters).hitbox)).x;
+     y=(snd characters).velocity.y + (fst ((snd characters).hitbox)).y} in
   moveto (fst characters) newpos1 ; moveto (snd characters) newpos2 ;
   let newvy1 = if (fst characters).air then
                  if (fst characters).velocity.y = (fst characters).speed * fallconstant then
@@ -96,7 +160,7 @@ let process_move (m: move) (i: int) : unit = (**consider stuns*)
       change_velocity (snd characters) newv ;
       let (a,b,c,d) = !lastmove in
       lastmove := (a,b,MRight,12))
-  | MDown -> (**what about when on stage*)
+  | MDown ->
     if i = 0 then
       (let newv = {x=(fst characters).velocity.x;y=(fst characters).speed * fallconstant} in
       change_velocity (fst characters) newv ;
@@ -107,17 +171,29 @@ let process_move (m: move) (i: int) : unit = (**consider stuns*)
       change_velocity (snd characters) newv ;
       let (a,b,c,d) = !lastmove in
       lastmove := (a,b,MDown,12))
-  | MUp -> (**Should be setting jumps var in character*)
+  | MUp ->
     if i = 0 then
-      (let newv = {x=(fst characters).velocity.x;y=(fst characters).speed * jumpconstant} in
-      change_velocity (fst characters) newv ;
       let (a,b,c,d) = !lastmove in
-      lastmove := (MUp,12,c,d))
+      let jumpconstant = if a = MUp && b > 0 then jumpconstant*2 else jumpconstant in
+      let newv = {x=(fst characters).velocity.x;y=(fst characters).speed * jumpconstant} in
+      if (fst characters).jumps > 0 then
+        (change_velocity (fst characters) newv ;
+        (fst characters).jumps <- (fst characters).jumps -1 ;
+        (fst characters).air <- true ;
+        lastmove := (MUp,12,c,d))
+      else
+        ()
     else
-      (let newv = {x=(snd characters).velocity.x;y=(snd characters).speed * jumpconstant} in
-      change_velocity (snd characters) newv ;
       let (a,b,c,d) = !lastmove in
-      lastmove := (a,b,MUp,12))
+      let jumpconstant = if c = MUp && d>0 then jumpconstant*2 else jumpconstant in
+      let newv = {x=(snd characters).velocity.x;y=(snd characters).speed * jumpconstant} in
+      if (snd characters).jumps > 0 then
+        (change_velocity (snd characters) newv ;
+        (snd characters).jumps <- (snd characters).jumps -1 ;
+        (snd characters).air <- true ;
+        lastmove := (a,b,MUp,12))
+      else
+        ()
 
 
 let rec tickprocessor () =
